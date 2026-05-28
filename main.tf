58d9dee5604b280abd07c239770c3fbbd7ce920f
johnny <johnny>
2026-05-28 15:51:48 -0300

    Workspaces

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "formacao-devops-bucket-iac"
+  bucket = "formacao-devops-bucket-iac-${terraform.workspace}"
 
   tags = {
     Name = "primeiro bucket"
     Iac = true
+    context = "${terraform.workspace}"
   }
 }
