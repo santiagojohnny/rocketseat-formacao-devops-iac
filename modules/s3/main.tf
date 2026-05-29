4d6a880762a26eb43b9015c42b783f63540dfdbc
johnny <johnny>
2026-05-29 19:07:18 -0300

    Modularizando o S3

---
+++ modules/s3/main.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.s3_bucket_name}-${terraform.workspace}"
+  tags = {}
+}
---
+++ modules/s3/outputs.tf
@@ -0,0 +1,5 @@
+output "bucket_domain_name" {
+  value       = ""
+  sensitive   = false
+  description = "Nome de domínio do bucket S3"
+}
