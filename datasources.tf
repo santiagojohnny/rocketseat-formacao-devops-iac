cfaa0117a717c5add2c54bd7aed5ad914d93bcdd
johnny <johnny>
2026-05-29 16:19:31 -0300

    Variáveis

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,3 @@
 data "aws_s3_bucket" "s3_bucket" {
-  bucket = "formacao-devops-bucket-iac-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-${terraform.workspace}"
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "formacao-devops-bucket-iac-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-${terraform.workspace}"
 
   tags = {
     Name = "primeiro bucket"
