9420dfe09db82a6afeba57a971848ada618baec9
johnny <johnny>
2026-06-20 19:12:38 -0300

    Modularizando o Cloudfront

---
+++ modules/cloudfront/variables.tf
@@ -0,0 +1,11 @@
+variable "origin_id" {
+  type = string
+  default = ""
+  description = "Id do S3"
+}
+
+variable "bucket_domain_name" {
+  type = string
+  default = ""
+  description = "Domínio do S3"
+}
